9a9c3819dcc6e646630e97ad94831ef3c701263c
Anmol Garg <anmolgarg240@gmail.com>
2023-03-03 21:16:19 +0530

    db

--- books/Book.js
+++ books/Book.js
@@ -1,1 +1,23 @@
 const mongoose = require("mongoose");
+
+//When we define model using mongoose using mongoose, we define the structure of a collection of mongoDB
+//Defining a model:
+// We provide parameters to .model, name of model and definition of model
+//in definition of model we give attributes & fields of our books collection
+
+mongoose.model("Book", {
+    //Title, author, numberPages, publisher
+
+    title: {
+        type: String, 
+        require: true
+    },
+    author: {
+        type: String,
+        require: true
+    },
+    numberPages: {
+        type: Number,
+        require: false
+    }
+});
